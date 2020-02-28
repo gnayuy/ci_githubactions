cc3e3750977806f2cf4f18a8db3c3d7b1adfdded
gnayuy <gnayuy@gmail.com>
2020-02-28 10:40:05 -0800

    update workflow ci

--- tests/test.py
+++ tests/test.py
@@ -1,9 +1,18 @@
+import unittest
 from example_package import example
 
-def test_fahrToKelv():
-    '''
-    make sure freezing is calculated correctly
-    '''
+class TestExample(unittest.TestCase):
 
+    def setUp(self):
+        self.temp = 32
     
-    assert example.fahrToKelv(32) == 273.15, 'incorrect freezing point!'
+    def test_fahrToKelv(self):
+        '''
+        make sure freezing is calculated correctly
+        '''
+        self.assertAlmostEqual(example.fahrToKelv(self.temp),273.15,2)
+        
+#        assert example.fahrToKelv(32) == 273.15, 'incorrect freezing point!'
+
+if __name__ == '__main__':
+    unittest.main()
